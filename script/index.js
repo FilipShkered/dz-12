60f53ddfba059cf0c3a088b8e3a55da463555410
Filip <example@gmail.com>
2023-03-11 11:14:48 +0200

    second commit / validation added

--- script/index.js
+++ script/index.js
@@ -6,7 +6,12 @@ msgButton.addEventListener('click', () => {
   const newMsg = msgInput.value;
   const newListItem = document.createElement('li');
   const textNode = document.createTextNode(newMsg);
-  newListItem.appendChild(textNode);
-  todoList.appendChild(newListItem);
-  msgInput.value = '';
+  
+  if (msgInput.value) {
+    newListItem.appendChild(textNode);
+    todoList.appendChild(newListItem);
+    msgInput.value = '';
+  } else {
+    alert('хватит лениться, запланируй что-то!)')
+  }
 });
