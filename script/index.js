4ecd93bd038336f431a7688de87df1bb6dde9a19
Filip <example@gmail.com>
2023-03-17 15:57:34 +0200

    third commit

--- script/index.js
+++ script/index.js
@@ -1,17 +1,42 @@
-const msgInput = document.querySelector('#msgInput');
-const msgButton = document.querySelector('#msgButton');
-const todoList = document.querySelector('#todoList');
+const input = document.querySelector('#msgInput');
+const btn = document.querySelector('#msgButton');
+const ul = document.querySelector('#todoList');
 
-msgButton.addEventListener('click', () => {
-  const newMsg = msgInput.value;
-  const newListItem = document.createElement('li');
-  const textNode = document.createTextNode(newMsg);
-  
-  if (msgInput.value) {
-    newListItem.appendChild(textNode);
-    todoList.appendChild(newListItem);
-    msgInput.value = '';
-  } else {
-    alert('хватит лениться, запланируй что-то!)')
+btn.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  const todo = getData()
+
+  if (!isTodoValid(todo)) {
+    showError()
+    return
   }
-});
+
+  writeTodo(todo)
+
+  clear()
+};
+
+
+function getData() {
+  return { message: input.value}
+}
+
+function isTodoValid(todo) {
+return todo.message !== ''
+}
+
+function showError() {
+  alert('Поле не может быть пустым')
+}
+
+function writeTodo(todo) {
+  const li = document.createElement('li')
+  li.textContent = todo.message
+  
+  ul.append(li)
+}
+
+function clear() {
+  input.value = ''
+}
